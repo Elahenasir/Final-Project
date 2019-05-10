9b1ab9dc65338195008c52f1cbe5b49d168f62c7
unknown <nasirelahe@gmail.com>
2019-05-10 10:55:08 -0700

    second-commit

---
+++ api/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+const class2 = require("./class2");
+const questions = require("./questions");
+router.use("/", class2);
+router.use("/", questions);
+
+module.exports = router;
